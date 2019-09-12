ed7539082afbdf4d13490852803f2feef8b30c6e
Roberto Aguilar <r@rreboto.com>
2019-09-12 13:53:55 -0400

    Add interactive shell util functions

--- src/git_smash/utils.py
+++ src/git_smash/utils.py
@@ -1,6 +1,9 @@
+import logging
 import sh
 import shlex
 import string
+
+SH_ERROR_1 = getattr(sh, 'ErrorReturnCode_1')
 
 
 def get_proc(command: str, **kwargs):
@@ -15,3 +18,25 @@ def run_command(command: str) -> str:
     proc = get_proc(command)
 
     return proc.stdout.decode('utf8').strip()
+
+
+def run_command_with_interactive_fallback(command: str, message: str = None):
+    logger = logging.getLogger(f'{__name__}')
+
+    try:
+        run_command(command)
+    except SH_ERROR_1 as exc:
+        message = message or ''
+
+        logger.error(f'could not run "{command}".  {message}')
+
+        run_interactive_shell()
+
+def run_interactive_shell(message: str = None):
+    logger = logging.getLogger(f'{__name__}')
+
+    message = message or f'launching a subshell.  when done, exit the shell'
+
+    logger.info(message)
+
+    return sh.bash('-i', _fg=True)
